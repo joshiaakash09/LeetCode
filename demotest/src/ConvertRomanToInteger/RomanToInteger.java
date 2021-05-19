ecb92470257103ed4d356cf562d35a52d7656e9e
pheonix <aakash_joshi@outlook.com>
2021-05-19 18:38:25 -0500

    Longest Common prefix

---
+++ demotest/src/ConvertRomanToInteger/RomanToInteger.java
@@ -0,0 +1,28 @@
+package ConvertRomanToInteger;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToInteger {
+    public static void main(String s) {
+        Map < Character, Integer > map = new HashMap();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int result = 0;
+        for (int i =0; i< s.length(); i++) {
+            if (i > 0 && map.get(s.CharAt(i)) > map.get(s.CharAt(i-1))){
+                result +=map.get(s.CharAt(i)) - 2 * map.get(s.CharAt(i-1));
+            } else {
+                result += map.get(s.CharAt(i));
+            }
+        }
+
+    }
+    return result;
+}
